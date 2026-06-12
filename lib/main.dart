be65b8d875519d7f77aa6b822c5c5d650d796e11
design-indra <ghostersoloist@gmail.com>
2026-06-12 18:26:41 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'core/theme/app_theme.dart';
 import 'presentation/navigation/app_router.dart';
 import 'presentation/providers/settings_provider.dart';
@@ -7,6 +8,7 @@ import 'presentation/screens/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id', null);
   runApp(const ProviderScope(child: KasirKuApp()));
 }
 
