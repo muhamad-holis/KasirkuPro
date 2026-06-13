9e8e9d5ab2d3cc7cf6dc0325b570ba6126ce95c5
design-indra <ghostersoloist@gmail.com>
2026-06-13 16:01:13 +0700

    Update auth_provider.dart

--- lib/presentation/providers/auth_provider.dart
+++ lib/presentation/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:drift/drift.dart'; // <-- Import tambahan untuk Value()
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../data/database/app_database.dart';
